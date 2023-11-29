ccac9025893e235347c26fa9df09279059cf9fee
Samuel fernandes <samuelfernandes10001@gamil.com>
2023-11-29 12:32:32 +0530

    deploy

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from "express";
 import ejs from "ejs";
-
+import  bodyParser  from "body-parser";
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let title = [];
+let message = [];
 
 app.get("/", (req,res)=>{
-    res.render("home")
+    res.render("home", {title: title, message: message})
+})
+
+app.post("/", (req, res)=>{
+    title.push(req.body["title"])
+    message.push(req.body["message"])
+    res.redirect("/")
 })
 
 app.listen(port, ()=>{
