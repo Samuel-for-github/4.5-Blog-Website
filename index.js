595f3a83d3f2dadfd6e7df84c9ce116f5f2c8dab
Samuel fernandes <samuelfernandes10001@gamil.com>
2023-11-29 12:11:55 +0530

    gi

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import ejs from "ejs";
 
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req,res)=>{
-    res.render("home.ejs")
+    res.render("home")
 })
 
 app.listen(port, ()=>{
